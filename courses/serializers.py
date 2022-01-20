ac899a4093f1f1d4dd5978afe96690509072f469
ayush <gawaias@rknec.edu>
2022-01-20 21:35:21 +0530

    added comment in serializers.py

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Course
 
+#Comment here
 class CourseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Course
